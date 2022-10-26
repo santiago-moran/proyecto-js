37a8dab50a3141b425108f2049778b1ff5412742
Santiago Moran <santim366@gmail.com>
2022-10-26 19:13:24 -0300

    nuevo desafio

--- ejericio.js
+++ ejericio.js
@@ -1,114 +1,179 @@
-let nombre = prompt('Ingrese su nombre: ')
-let msjFinal = 'Gracias por usar el simulador. Recargue la página para elegir otra opcion diferente.'
-alert('Hola ' + nombre + ', bienvenido a este simulador, correspondiente a la primer entrega del proyecto de CoderHouse. Que lo disfrutes.')
-
-function pedirDatos() {
-    let opcion = parseInt(prompt('1. Calcular nota final de alumnos.\n2. Conocer la tabla de multiplicar de un numero.\n3. Precio total de productos seleccionados.\n\nDigitar un numero, segun la opcion elegida: '))
-    return opcion
-}
-
-function mostrarDatos() {
-    let opciones = pedirDatos()
-    switch (opciones) {
-        case 1:
-            alert('Usted ha elegido la opcion 1: calcular nota final de alumnos. Que lo disfrute :)')
-            let cantAlumnos = parseInt(prompt('Ingrese la cantidad de alumnos que desea usar: '))
-            while(cantAlumnos<=0) {
-                cantAlumnos = parseInt(prompt('Cantidad invalida, debe ser mayor a 0. Ingrese nuevamente la cantidad: '))
-            }
-            for (i = 1; i <= cantAlumnos; i++) {
-                let nota1 = parseFloat(prompt(`Alumno ${i}, ingrese su primer nota: `))
-                while (nota1 < 1 || nota1 > 10) {
-                    nota1 = parseFloat(prompt('Nota inválida. Debe ser del 1 al 10. Ingrese nuevamente la nota: '))
-                }
-                let nota2 = parseFloat(prompt(`Alumno ${i}, ingrese su segunda nota: `))
-                while (nota2 < 1 || nota2 > 10) {
-                    nota2 = parseFloat(prompt('Nota inválida. Debe ser del 1 al 10. Ingrese nuevamente la nota: '))
-                }
-                let nota3 = parseFloat(prompt(`Alumno ${i}, ingrese su tercer nota: `))
-                while (nota3 < 1 || nota3 > 10) {
-                    nota3 = parseFloat(prompt('Nota inválida. Debe ser del 1 al 10. Ingrese nuevamente la nota: '))
-                }
-                let sumaNota = nota1 + nota2 + nota3
-                let promedio = sumaNota / 3
-                if (promedio >= 4) {
-                    alert(`Felicidades, su promedio final es ${promedio.toFixed(2)}. Usted aprobó la materia`)
-                } else {
-                    alert(`Lo sentimos mucho, su promedio es ${promedio.toFixed(2)}. Usted desaprobó la materia`)
-                }
-            }
-            alert(msjFinal)
-            break;
-        case 2:
-            alert('Usted ha elegido la opcion 2: conocer la tabla de multiplicar de un número. Que lo disfrute :)')
-            let numTabla = parseInt(prompt('Digite el numero del cual desea saber la tabla: '))
-            while (numTabla<0) {
-                numTabla = prompt('El numero debe ser mayor o igual a cero. Ingrese nuevamente un numero: ')
-            }
-            console.log(`Tabla del ${numTabla}: \n\n`)
-            for (let i = 0; i<=10;i++) {
-                let resultado = numTabla*i
-                console.log(`${numTabla} X ${i} = ${resultado}`)
-            }
-            let siNo = prompt('Desea saber la tabla de multiplicar del resto de los números, del 1 al 10? (Responder si o no): ')
-            if (siNo.toLowerCase() == 'si') {
-                for (i = 1; i<=10; i++) {
-                    if (i == numTabla) {
-                        continue
-                    }
-                    console.log(`\n\nTabla del ${i}: \n\n`)
-                    for (let j = 0; j <= 10; j++) {
-                        let resultado2 = i*j
-                        console.log(`${i} x ${j} = ${resultado2}`)
-                    }
-                }
-                alert(msjFinal)
-            }
-            else {
-                alert(msjFinal)
-            }
-            break;
-        case 3:
-            let contadorProductos = ""
-            let sumaTotal = 0
-            alert('Usted ha elegido la opcion 3: precio total de productos seleccionados. Que lo disfrute :)')
-            let agregarProducto = prompt('Desea agregar un producto a su carrito? (Responda si o no)')
-            if (agregarProducto.toLowerCase()=='si') {
-                let nombreProducto = prompt ('Ingrese el nombre del producto: ')
-                let precioProducto = parseInt(prompt('Ingrese el precio del producto (Sin el signo "$"): '))
-                while(precioProducto<=0) {
-                    precioProducto = parseInt(prompt('Precio invalido, debe ser mayor a 0. Ingrese nuevamente el precio: '))
-                }
-                sumaTotal += precioProducto
-                contadorProductos +=  nombreProducto + ' = ' + '$' + precioProducto
-                agregarProducto = prompt('Desea seguir agregando productos? (Responda si o no)')
-                if (agregarProducto.toLowerCase()=='no') {
-                    contadorProductos = nombreProducto + ' = ' + '$' + precioProducto
-                    alert(`Productos elegidos\n\n${contadorProductos}.\n\nEl monto total es de $${sumaTotal}`)
-                    alert(msjFinal)
-                }
-                else {
-                    while (agregarProducto.toLowerCase()=='si') {
-                        nombreProducto = prompt ('Ingrese el nombre del producto: ')
-                        precioProducto = parseInt(prompt('Ingrese el precio del producto(Sin el signo "$"): '))
-                        while(precioProducto<=0) {
-                            precioProducto = parseInt(prompt('Precio invalido, debe ser mayor a 0. Ingrese nuevamente el precio: '))
-                        }
-                        sumaTotal += precioProducto
-                        contadorProductos += ', ' + nombreProducto + ' = ' + '$' + precioProducto
-                        agregarProducto = prompt('Desea seguir agregando productos? (Responda si o no)')
-                    }
-                    alert(`Productos elegidos\n\n${contadorProductos}.\n\nEl monto total es de $${sumaTotal}`)
-                    alert(msjFinal)
-                }
-            }
-            else {
-                alert(msjFinal)
-            }
-            break;
-        default:
-            alert('Error en el programa. Número no válido. Debe recargar la página')
-            break;
+/* VARIABLES GENERALES */
+class Libros {
+    constructor(autor, titulo, precio) {
+        this.autor = autor
+        this.titulo = titulo
+        this.precio = precio
+    }
+    mostrarLibro () {
+        console.log (`${this.titulo.toUpperCase()}, de ${this.autor.toUpperCase()}. Precio: $${this.precio}`)
     }
 }
-mostrarDatos()
+const libro1 = new Libros(`Santiago Moran`, `Las Pulgas Locas`, 1500)
+const libro2 = new Libros(`Agustina Rivero`, `Toxicidad Pura`, 1850)
+const libro3 = new Libros(`Ignacio Moran`, `Lucha con los Brazos`, 1435)
+const libro4 = new Libros(`Zoel Reising`, `El Mal de Amores`, 1100)
+const libro5 = new Libros(`Paula Guinard`, `Folklore para Tod@s`, 1320)
+
+const biblioteca = [libro1, libro2, libro3, libro4, libro5]
+
+/* VARIABLES LOCALES */
+function presentacion() {
+    let nombre = prompt((`Bienvenido. Ingrese su nombre, por favor: `))
+    alert(`Hola ${nombre}. Espero que puedas disfrutar este simulador :)`)
+}
+
+function opcionSwitch() {
+    let elegirOpcion = parseInt(prompt(`Por favor, elija una opcion: \n\n1. Ver catálogo de libros\n2. Agregar un nuevo libro al catálogo\n3. Buscar libro por titulo\n4. Buscar libro por precio\n5. Buscar libro por autor\n6. Ver titulos incluidos en el catálogo\n7. Ordenar catalogo de menor a mayor precio\n8. Finalizar programa`))
+    switch (elegirOpcion) {
+        case 1:
+            verCatalogoLibros()
+            break;
+        case 2:
+            agregarNuevoLibro()
+            break;
+        case 3:
+            buscarTitulo()
+            break;
+        case 4:
+            buscarPrecio()
+            break;
+        case 5:
+            buscarAutor()
+            break;
+        case 6: 
+            verTitulos()
+            break;
+        case 7:
+            ordenarMenorMayor()
+            break;
+        case 8: 
+        alert(`PROCEDIENDO A SALIR DEL PROGRAMA`)
+        break;
+        default:
+            alert(`OPCION INEXISTENTE`)
+            opcionSwitch()
+        break;
+    }
+}
+
+function verCatalogoLibros() {
+    console.log(`Catalogo de libros`)
+    for (const element of biblioteca) {
+        element.mostrarLibro()
+    }
+    console.log(` \n`)
+    alert(`Mostrando lista en consola`)
+    alert(`Volviendo al menú principal...`)
+    opcionSwitch()
+}
+
+function agregarNuevoLibro() {
+    let nuevoTitulo = prompt(`Por favor, ingrese el título de su nuevo libro: `)
+    let nuevoAutor = prompt(`Por favor, ingrese el autor del su nuevo libro: `)
+    let nuevoPrecio = parseFloat(prompt(`Por favor, ingrese el precio del su nuevo libro: `))
+    const libroNuevo = new Libros(nuevoAutor, nuevoTitulo, nuevoPrecio)
+    biblioteca.push(libroNuevo)
+    alert(`Mostrando nuevo catalogo en consola`)
+    console.log(`Nuevo catálogo de libros: `)
+    for (const elemento of biblioteca) {
+        elemento.mostrarLibro()
+    }
+    console.log(" \n")
+    alert(`Volviendo al menú principal...`)
+    opcionSwitch()
+}
+
+function buscarTitulo() {
+    let nombreTitulo = prompt(`Ingrese el titulo del libro que desea buscar: `)
+    let filtrarTitulo = biblioteca.filter((libro) => libro.titulo.toLowerCase().includes(nombreTitulo.toLowerCase()))
+    while (filtrarTitulo.length == 0) {
+        nombreTitulo = prompt(`El titulo ${nombreTitulo} no se encuentra en el catálogo. Ingrese otro titulo o digite SALIR para terminar la búsqueda: `)
+        if (nombreTitulo.toUpperCase() == "SALIR") {
+            alert(`Volviendo al menú principal...`)
+            opcionSwitch()
+        } else {
+            filtrarTitulo = biblioteca.filter((libro) => libro.titulo.includes(nombreTitulo))
+        }
+    }
+    if (filtrarTitulo.length == 0) {
+        alert(`BUSQUEDA FINALIZADA: NINGUN LIBRO ENCONTRADO`)
+        alert(`Volviendo al menú principal...`)
+        opcionSwitch()
+    } else {
+        console.log(`Se ha encontrado el siguiente libro con ese titulo: `)
+        filtrarTitulo.forEach( element => element.mostrarLibro())
+        console.log(` \n`)
+        alert(`Volviendo al menú principal...`)
+        opcionSwitch()
+    }
+}
+
+function buscarPrecio() {
+    let precioLibro = parseFloat(prompt(`Ingrese el precio del libro que desea buscar: `))
+    let filtrarPrecio = biblioteca.filter((libro) => libro.precio == (precioLibro))
+    while (filtrarPrecio.length == 0) {
+        precioLibro = parseFloat(prompt(`El precio indicado no coincide con ningun libro en el catálogo. Ingrese otro precio o digite 0 para terminar la búsqueda: `))
+        if (precioLibro == 0) {
+            alert(`Volviendo al menú principal...`)
+            opcionSwitch()
+        } else {
+            filtrarPrecio = biblioteca.filter((libro) => libro.precio == (parseFloat(precioLibro)))
+        }
+    }
+    if (filtrarPrecio.length == 0) {
+        alert(`BUSQUEDA FINALIZADA: NINGUN LIBRO ENCONTRADO`)
+        alert(`Volviendo al menú principal...`)
+        opcionSwitch()
+    } else {
+        console.log(`Se ha encontrado el siguiente libro con ese precio: `)
+        filtrarPrecio.forEach( element => element.mostrarLibro())
+        console.log(` \n`)
+        alert(`Volviendo al menú principal...`)
+        opcionSwitch()
+    }
+}
+
+function buscarAutor() {
+    let nombreAutor = prompt(`Ingrese el nombre del autor del libro que desea buscar: `)
+    let filtrarNombre = biblioteca.filter((libro) => libro.autor.toLowerCase().includes(nombreAutor.toLowerCase()))
+    while (filtrarNombre.length == 0) {
+        nombreAutor = prompt(`El autor ${nombreAutor} no se encuentra en el catálogo. Ingrese otro autor o digite SALIR para terminar la búsqueda: `)
+        if (nombreAutor.toUpperCase() == "SALIR") {
+            alert(`Volviendo al menú principal...`)
+            opcionSwitch()
+        } else {
+            filtrarNombre = biblioteca.filter((libro) => libro.autor.includes(nombreAutor))
+        }
+    }
+    if (filtrarNombre.length == 0) {
+        alert(`BUSQUEDA FINALIZADA: NINGUN LIBRO ENCONTRADO`)
+        alert(`Volviendo al menú principal...`)
+        opcionSwitch()
+    } else {
+        console.log(`Se ha encontrado el siguiente libro con ese autor: `)
+        filtrarNombre.forEach( element => element.mostrarLibro())
+        console.log(` \n`)
+        alert(`Volviendo al menú principal...`)
+        opcionSwitch()
+    }
+}
+function verTitulos() {
+    console.log(`Titulos incluidos en el catálogo:`)
+    biblioteca.forEach( book => {console.log(book.titulo.toUpperCase())})
+    console.log(` \n`)
+    alert(`Volviendo al menú principal...`)
+    opcionSwitch()
+}
+function ordenarMenorMayor() {
+    console.log(`Catalogo ordenado de menor a mayor precio:`)
+    biblioteca.sort( (a, b) => a.precio - b.precio )
+    for (const books of biblioteca) {
+        books.mostrarLibro()
+    }
+    console.log(`\n`)
+    alert(`Volviendo al menú principal...`)
+    opcionSwitch()
+}
+presentacion()
+opcionSwitch()
