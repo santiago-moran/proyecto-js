da6cf4edc592a15b61579e09d5bc0518bd4067be
Santiago Moran <santim366@gmail.com>
2022-10-26 21:33:34 -0300

    ultimos retoques

--- ejericio.js
+++ ejericio.js
@@ -1,8 +1,8 @@
 /* VARIABLES GENERALES */
 class Libros {
     constructor(autor, titulo, precio) {
-        this.autor = autor
-        this.titulo = titulo
+        this.autor = autor.toUpperCase()
+        this.titulo = titulo.toUpperCase()
         this.precio = precio
     }
     mostrarLibro () {
@@ -24,7 +24,7 @@ function presentacion() {
 }
 
 function opcionSwitch() {
-    let elegirOpcion = parseInt(prompt(`Por favor, elija una opcion: \n\n1. Ver catálogo de libros\n2. Agregar un nuevo libro al catálogo\n3. Buscar libro por titulo\n4. Buscar libro por precio\n5. Buscar libro por autor\n6. Ver titulos incluidos en el catálogo\n7. Ordenar catalogo de menor a mayor precio\n8. Finalizar programa`))
+    let elegirOpcion = parseInt(prompt(`Por favor, elija una opcion: \n\n1. Ver catálogo de libros\n2. Agregar un nuevo libro al catálogo\n3. Buscar libro por titulo\n4. Buscar libro por precio\n5. Buscar libro por autor\n6. Ver titulos incluidos en el catálogo\n7. Ordenar catalogo de menor a mayor precio\n8. Eliminar un libro del catalogo\n9. Finalizar programa`))
     switch (elegirOpcion) {
         case 1:
             verCatalogoLibros()
@@ -47,9 +47,12 @@ function opcionSwitch() {
         case 7:
             ordenarMenorMayor()
             break;
-        case 8: 
-        alert(`PROCEDIENDO A SALIR DEL PROGRAMA`)
-        break;
+        case 8:
+            eliminarLibro()
+            break;
+        case 9: 
+            alert(`PROCEDIENDO A SALIR DEL PROGRAMA`)
+            break;
         default:
             alert(`OPCION INEXISTENTE`)
             opcionSwitch()
@@ -58,7 +61,7 @@ function opcionSwitch() {
 }
 
 function verCatalogoLibros() {
-    console.log(`Catalogo de libros`)
+    console.log(`Catalogo de libros:`)
     for (const element of biblioteca) {
         element.mostrarLibro()
     }
@@ -70,8 +73,13 @@ function verCatalogoLibros() {
 
 function agregarNuevoLibro() {
     let nuevoTitulo = prompt(`Por favor, ingrese el título de su nuevo libro: `)
-    let nuevoAutor = prompt(`Por favor, ingrese el autor del su nuevo libro: `)
-    let nuevoPrecio = parseFloat(prompt(`Por favor, ingrese el precio del su nuevo libro: `))
+    const tituloRep = biblioteca.some( element => element.titulo == nuevoTitulo)
+    while (tituloRep == true) {
+        nuevoTitulo = prompt(`Ese titulo ya existe. Por favor, ingrese otro titulo:`)
+        tituloRep = biblioteca.some( element => element.titulo == nuevoTitulo)
+    }
+    let nuevoAutor = prompt(`Por favor, ingrese el autor de su nuevo libro: `)
+    let nuevoPrecio = parseInt(prompt(`Por favor, ingrese el precio de su nuevo libro: `))
     const libroNuevo = new Libros(nuevoAutor, nuevoTitulo, nuevoPrecio)
     biblioteca.push(libroNuevo)
     alert(`Mostrando nuevo catalogo en consola`)
@@ -88,13 +96,8 @@ function buscarTitulo() {
     let nombreTitulo = prompt(`Ingrese el titulo del libro que desea buscar: `)
     let filtrarTitulo = biblioteca.filter((libro) => libro.titulo.toLowerCase().includes(nombreTitulo.toLowerCase()))
     while (filtrarTitulo.length == 0) {
-        nombreTitulo = prompt(`El titulo ${nombreTitulo} no se encuentra en el catálogo. Ingrese otro titulo o digite SALIR para terminar la búsqueda: `)
-        if (nombreTitulo.toUpperCase() == "SALIR") {
-            alert(`Volviendo al menú principal...`)
-            opcionSwitch()
-        } else {
-            filtrarTitulo = biblioteca.filter((libro) => libro.titulo.includes(nombreTitulo))
-        }
+        nombreTitulo = prompt(`El titulo ${nombreTitulo} no se encuentra en el catálogo. Ingrese otro titulo: `)
+        filtrarTitulo = biblioteca.filter((libro) => libro.titulo.includes(nombreTitulo))
     }
     if (filtrarTitulo.length == 0) {
         alert(`BUSQUEDA FINALIZADA: NINGUN LIBRO ENCONTRADO`)
@@ -113,13 +116,8 @@ function buscarPrecio() {
     let precioLibro = parseFloat(prompt(`Ingrese el precio del libro que desea buscar: `))
     let filtrarPrecio = biblioteca.filter((libro) => libro.precio == (precioLibro))
     while (filtrarPrecio.length == 0) {
-        precioLibro = parseFloat(prompt(`El precio indicado no coincide con ningun libro en el catálogo. Ingrese otro precio o digite 0 para terminar la búsqueda: `))
-        if (precioLibro == 0) {
-            alert(`Volviendo al menú principal...`)
-            opcionSwitch()
-        } else {
-            filtrarPrecio = biblioteca.filter((libro) => libro.precio == (parseFloat(precioLibro)))
-        }
+        precioLibro = parseFloat(prompt(`El precio indicado no coincide con ningun libro en el catálogo. Ingrese otro precio: `))
+        filtrarPrecio = biblioteca.filter((libro) => libro.precio == (parseFloat(precioLibro)))
     }
     if (filtrarPrecio.length == 0) {
         alert(`BUSQUEDA FINALIZADA: NINGUN LIBRO ENCONTRADO`)
@@ -138,25 +136,20 @@ function buscarAutor() {
     let nombreAutor = prompt(`Ingrese el nombre del autor del libro que desea buscar: `)
     let filtrarNombre = biblioteca.filter((libro) => libro.autor.toLowerCase().includes(nombreAutor.toLowerCase()))
     while (filtrarNombre.length == 0) {
-        nombreAutor = prompt(`El autor ${nombreAutor} no se encuentra en el catálogo. Ingrese otro autor o digite SALIR para terminar la búsqueda: `)
-        if (nombreAutor.toUpperCase() == "SALIR") {
-            alert(`Volviendo al menú principal...`)
-            opcionSwitch()
-        } else {
-            filtrarNombre = biblioteca.filter((libro) => libro.autor.includes(nombreAutor))
+        nombreAutor = prompt(`El autor ${nombreAutor} no se encuentra en el catálogo. Ingrese otro autor: `)
+        filtrarNombre = biblioteca.filter((libro) => libro.autor.toUpperCase().includes(nombreAutor.toUpperCase()))
         }
-    }
     if (filtrarNombre.length == 0) {
-        alert(`BUSQUEDA FINALIZADA: NINGUN LIBRO ENCONTRADO`)
         alert(`Volviendo al menú principal...`)
         opcionSwitch()
-    } else {
+    } 
+    else {
         console.log(`Se ha encontrado el siguiente libro con ese autor: `)
         filtrarNombre.forEach( element => element.mostrarLibro())
-        console.log(` \n`)
-        alert(`Volviendo al menú principal...`)
-        opcionSwitch()
     }
+    console.log(` \n`)
+    alert(`Volviendo al menú principal...`)
+    opcionSwitch()
 }
 function verTitulos() {
     console.log(`Titulos incluidos en el catálogo:`)
@@ -174,6 +167,41 @@ function ordenarMenorMayor() {
     console.log(`\n`)
     alert(`Volviendo al menú principal...`)
     opcionSwitch()
+}
+function eliminarLibro() {
+    let band = false
+    let deleteBook = prompt(`Ingrese el titulo del libro que desea eliminar:`)
+    for (const el of biblioteca) {
+        if (el.titulo.toUpperCase() == deleteBook.toUpperCase()) {
+            let preguntaSegura = prompt(`Esta seguro/a que desea eliminar este libro? (Responder si o no)`)
+            while (preguntaSegura != 'si' && preguntaSegura != 'no') {
+                preguntaSegura = prompt(`Debe responder si o no. Ingrese nuevamente una respuesta:`)
+            }
+            if (preguntaSegura.toLowerCase() == "si") {
+                band = true
+                biblioteca.splice(biblioteca.indexOf(el), 1)
+            }
+            else if (preguntaSegura.toLowerCase() == "no") {
+                eliminarLibro()
+            }
+        }
+    }
+    while (band == false) {
+        deleteBook = prompt(`No se ha encontrado ningun libro con ese titulo. Ingrese un nuevo titulo:`)
+        for (const el of biblioteca) {
+            if (el.titulo.toUpperCase() == deleteBook.toUpperCase()) {
+                band = true
+                biblioteca.splice(biblioteca.indexOf(el), 1)
+            }
+        }
+    }
+    if (band == true) {
+        console.log(`Nuevo catalogo de libros:`)
+        biblioteca.forEach( element => element.mostrarLibro())
+        console.log(` \n`)
+        alert(`Volviendo al menu principal...`)
+        opcionSwitch()
+    }
 }
 presentacion()
 opcionSwitch()
